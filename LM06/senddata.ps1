b4b4907c3986973264f274be4c41507b977b6e37
RachelGehrke <81997941+RachelGehrke@users.noreply.github.com>
2024-09-30 13:20:03 -0500

    Add files via upload

---
+++ LM06/senddata.ps1
@@ -0,0 +1,14 @@
+﻿$ip = "KALIVMIP"
+$port = 9999
+$message = "hello from windows!"
+
+$client = new-object System.Net.Sockets.TcpClient($ip,$port)
+
+$stream = $client.getstream()
+$writer = New-Object system.io.streamwriter($stream)
+$writer.writeline($message)
+$writer.flush()
+
+$reader= new-object system.io.streamreader($stream)
+$response = $reader.readline()
+write-output "Received: $response"
